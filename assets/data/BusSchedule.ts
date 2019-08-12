a9147f29905b3a975b96d6438bb75795f8c73b18
Brian-Holmes <52579801+Brian-Holmes@users.noreply.github.com>
2019-08-12 13:06:00 -0400

    Add files via upload

---
+++ assets/data/BusSchedule.ts
@@ -0,0 +1,12 @@
+import {ScheduleInfo} from '../../app/scheduleInfo';
+
+export const BUSSCHEDULE: ScheduleInfo[] = [
+    {route : 'Oakville-MIssissauga-Toronto', 
+    leaveTime:'9am', 
+    arriveTime: '10:30am'
+    },
+    {route : 'Orillia-Barrie-Toronto', 
+    leaveTime:'9am', 
+    arriveTime: '12pm'
+    }
+];
